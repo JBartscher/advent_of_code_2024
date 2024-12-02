b21d0b10518dc7c9bb9b08fac55a533bd3372840
Jasper Bartscher <jasper.bartscher@efs-techhub.com>
2024-12-02 10:52:35 +0100

    Day one

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::path::Path;
+
+mod day_one;
+
+fn main() {
+    println!("Day one");
+    day_one::first_task();
+    day_one::second_task();
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+pub fn read_input(path: &str) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+
+    if let Ok(lines) = read_lines(path) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines.flatten() {
+            // println!("{}", line);
+            result.push(line);
+        }
+    }
+
+    result
+}
