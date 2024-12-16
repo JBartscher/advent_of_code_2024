01ee0dd01cb79fc59052ee8c97a4c500b655f8b1
Jasper Bartscher <jasper.bartscher@efs-techhub.com>
2024-12-16 10:26:04 +0100

    Add tests for day 14

--- src/day_fourteen.rs
+++ src/day_fourteen.rs
@@ -31,22 +31,18 @@ impl Robot {
         let new_x = (self.x as isize) + self.v_x;
         if new_x < 0 {
             self.x = ((width as isize + 1) + new_x) as usize;
-        }
-        else if new_x > width as isize {
-            self.x = (new_x - (width as isize -1)) as usize;
-        }
-        else {
+        } else if new_x > width as isize {
+            self.x = (new_x - (width as isize + 1)) as usize;
+        } else {
             self.x = new_x as usize;
         }
 
         let new_y = (self.y as isize) + self.v_y;
         if new_y < 0 {
             self.y = ((height as isize + 1) + new_y) as usize;
-        }
-        else if new_y > height as isize {
-            self.y = (new_y - (height as isize) -1) as usize;
-        }
-        else {
+        } else if new_y > height as isize {
+            self.y = (new_y - (height as isize + 1)) as usize;
+        } else {
             self.y = new_y as usize;
         }
     }
@@ -57,6 +53,31 @@ impl Robot {
             return true;
         }
         false
+    }
+
+    pub fn get_quadrant(&self, width: usize, height: usize) -> usize {
+        let half_width = width / 2;
+        let half_height = height / 2;
+
+        let x = match self.x {
+            n if (0..half_width).contains(&n) => 0,
+            n if (half_width..=width).contains(&n) => 1,
+            _ => -1
+        };
+
+        let y = match self.y {
+            n if (0..half_height).contains(&n) => 0,
+            n if (half_height..=height).contains(&n) => 1,
+            _ => -1
+        };
+
+        match (x, y) {
+            (0, 0) => 1,
+            (1, 0) => 2,
+            (0, 1) => 3,
+            (1, 1) => 4,
+            _ => 0
+        }
     }
 }
 
@@ -87,4 +108,87 @@ pub fn first_task() {
 
 pub fn second_task() {
     println!("Answer 2/2: {}", 0);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day_fourteen::Robot;
+
+    #[test]
+    fn tick_negative_wrap_around() {
+        let mut r = Robot {
+            x: 0,
+            y: 1,
+            v_x: -1,
+            v_y: -2,
+        };
+        r.tick(10, 10);
+        assert_eq!(r.x, 10);
+        assert_eq!(r.y, 10);
+    }
+
+    #[test]
+    fn tick_positive_wrap_around() {
+        let mut r = Robot {
+            x: 9,
+            y: 5,
+            v_x: 5,
+            v_y: 8,
+        };
+        r.tick(10, 10);
+        assert_eq!(r.x, 3);
+        assert_eq!(r.y, 2);
+    }
+
+    #[test]
+    fn tick() {
+        let mut r = Robot {
+            x: 7,
+            y: 3,
+            v_x: 2,
+            v_y: -2,
+        };
+        r.tick(10, 10);
+        assert_eq!(r.x, 9);
+        assert_eq!(r.y, 1);
+    }
+
+    #[test]
+    fn get_quadrant() {
+        let mut r = Robot {
+            x: 1,
+            y: 3,
+            v_x: 2,
+            v_y: -2,
+        };
+
+        assert_eq!(r.get_quadrant(10, 10), 1);
+
+        let mut r = Robot {
+            x: 7,
+            y: 3,
+            v_x: 2,
+            v_y: -2,
+        };
+
+        assert_eq!(r.get_quadrant(10, 10), 2);
+
+        let mut r = Robot {
+            x: 1,
+            y: 10,
+            v_x: 2,
+            v_y: -2,
+        };
+
+        assert_eq!(r.get_quadrant(10, 10), 3);
+
+        let mut r = Robot {
+            x: 8,
+            y: 10,
+            v_x: 2,
+            v_y: -2,
+        };
+
+        assert_eq!(r.get_quadrant(10, 10), 4);
+    }
 }
